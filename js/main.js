87e5945a1102b48891695a17577cb059a7b33c0b
Ariyan0047 <aariyan0047@gmail.com>
2020-12-04 21:11:25 +0600

    SIXTH COMMIT

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const addItemTask = document.querySelector(".taskListForm")
 const userInput = document.querySelector("#userInput")
 const lists = document.querySelector(".toDoList")
+const popUp = document.querySelector(".popUp")
+const search = document.querySelector("#searchList")
+const deleteBtn = document.querySelector(".delete")
+const cancelBtn = document.querySelector(".cancel")
 
 const generateTemplate = (todo) => {
     const generateHtml = `<li class="toDoListItem">${todo}</li>`
     lists.innerHTML += generateHtml
 }
-
 
 addItemTask.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -15,4 +18,41 @@ addItemTask.addEventListener("submit", (event) => {
         generateTemplate(toDo)
     }
     addItemTask.reset()
+})
+
+lists.addEventListener("click", event => {
+    event.preventDefault()
+    if (event.target.classList.contains("toDoListItem")) {
+        popUp.style.display = "block"
+
+        deleteBtn.addEventListener("click", () => {
+            event.target.remove()
+            popUp.style.display = "none"
+        })
+
+        cancelBtn.addEventListener("click", () => {
+            popUp.style.display = "none"
+        })
+    }
+})
+
+const filteredListItems = (searchItem) => {
+    Array.from(lists.children).filter(item => {
+        return !item.textContent.toLowerCase().includes(searchItem)
+    }).forEach((item) => {
+        item.classList.add("filtered")
+    })
+
+
+    Array.from(lists.children).filter(item => {
+        return item.textContent.toLowerCase().includes(searchItem)
+    }).forEach((item) => {
+        item.classList.remove("filtered")
+    })
+
+}
+
+search.addEventListener("keyup", event => {
+    const searchItem = search.value.trim()
+    filteredListItems(searchItem)
 })
